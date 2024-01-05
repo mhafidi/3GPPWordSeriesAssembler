a0d77a19ff8b2f4d8b2ad6eaa475a2424c003c75
mehdi.hafidi <mehdi.hafidi@transatel.com>
2024-01-05 15:17:32 +0100

    initial commit

---
+++ java3wsa/src/main/java/com/gpp/WSAApplication.java
@@ -0,0 +1,10 @@
+package com.gpp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSAApplication {
+    public static void main(String[] args){
+
+    }
+}
